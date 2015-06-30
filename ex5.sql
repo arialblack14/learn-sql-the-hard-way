cb69db5d6c01725b840fe896755b2d2cd37a926a
arialblack14 <tsirasalexis@yahoo.gr>
2015-06-30 23:04:47 +0300

    Added exercise 5 solution

---
+++ ex5.sql
@@ -0,0 +1,9 @@
+SELECT * FROM person;
+SELECT name, age FROM pet;
+SELECT name, age FROM pet WHERE dead = 0;
+SELECT * FROM person WHERE first_name != "Zed";
+
+SELECT * FROM pet WHERE age > 10;
+SELECT * FROM person WHERE age < 35;
+SELECT * FROM person WHERE age > 35;
+SELECT * FROM person WHERE first_name = "Zed" AND age > 30;
